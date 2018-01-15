087f0b2ca2cae693efd227d7143cf11565534633
Rostyslav_Stakhiv <rostyk96@gmail.com>
2018-01-15 07:00:27 +0200

    - Converted to swift 4

---
+++ DatabaseExplorer/DatabaseExplorer/DataModel/DataModel.swift
@@ -0,0 +1,149 @@
+//
+//  DataModel.swift
+//  Mitwelt
+//
+//  Created by Rostyslav Stakhiv on 8/2/17.
+//  Copyright © 2017 Rostyslav Stakhiv. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataModel {
+    private var objectUniqueIndex = 0
+    
+    static let shared = DataModel()
+    
+    private init() {
+    }
+    
+    // MARK: Core Data Stack setup
+    private lazy var managedObjectContext: NSManagedObjectContext = {
+        
+        var managedObjectContext: NSManagedObjectContext?
+        
+        let coordinator = self.persistentStoreCoordinator
+        managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext?.persistentStoreCoordinator = coordinator
+        return managedObjectContext!
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = Bundle.main.url(forResource: "DatabaseExplorer", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.appendingPathComponent("DatabaseExplorer.sqlite")
+        var failureReason = "There was an error creating or loading the application's saved data."
+        do {
+            // Configure automatic migration.
+            let options = [ NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true ]
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: options)
+        } catch {
+            do {
+                try FileManager.default.removeItem(at: url)
+                try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
+            } catch {
+                // Report any error we got.
+                var dict = [String: AnyObject]()
+                dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject?
+                dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject?
+                
+                dict[NSUnderlyingErrorKey] = error as NSError
+                let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+                // Replace this with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+                abort()
+            }
+        }
+        
+        return coordinator
+    }()
+    
+    // MARK: private
+    private lazy var applicationDocumentsDirectory: URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named in the application's documents Application Support directory.
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[urls.count-1]
+    }()
+    
+    // MARK: public
+    func saveContext () {
+        if managedObjectContext.hasChanges {
+            do {
+                try managedObjectContext.save()
+            } catch {
+                //TODO: Handle error
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    //MARK: Context
+    var context: NSManagedObjectContext {
+        get {
+            return self.managedObjectContext
+        }
+    }
+    
+    
+    //MARK: Private Methods
+    private func recycleUniqueEntity(entity: ModelMapper.Type, id: Int) -> Any? {
+        guard let fetchedEntity = fetchUniqueEntity(request: entity.fetchRequest(id: id)) else {
+            return emptyObject(name: entity.entityName)
+        }
+        
+        return fetchedEntity
+    }
+    
+    private func fetchEntities(request: NSFetchRequest<NSFetchRequestResult>) -> [Any] {
+        do {
+            let fetchedObjects = try managedObjectContext.fetch(request)
+            return fetchedObjects
+        } catch {
+            fatalError("Failed to fetch employees: \(error)")
+        }
+    }
+    
+    private func fetchUniqueEntity(request: NSFetchRequest<NSFetchRequestResult>) -> Any? {
+        let fetchedObjects = fetchEntities(request: request)
+        assert(fetchedObjects.count <= 1, "only one object can exist with specified id")
+        
+        if fetchedObjects.count > 1 {
+            // try to recover by deleting all objects with given id
+            for fetchedObject in fetchedObjects {
+                managedObjectContext.delete(fetchedObject as! NSManagedObject)
+            }
+            
+            return nil
+        } else if fetchedObjects.count == 1 {
+            return fetchedObjects[0]
+        }
+        
+        return nil
+    }
+    
+    private func remove(entity: ModelMapper.Type){
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entity.entityName)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        do {
+            _ = try persistentStoreCoordinator.execute(deleteRequest, with: managedObjectContext)
+            managedObjectContext.reset()
+        } catch _ as NSError {
+            // TODO: handle the error
+        }
+    }
+    
+    private func emptyObject(name: String) -> NSManagedObject {
+        let entity = NSEntityDescription.entity(forEntityName: name, in: managedObjectContext)!
+        return NSManagedObject(entity:entity, insertInto:managedObjectContext)
+    }
+}
--- DatabaseExplorer/DatabaseExplorer/ViewController.swift
+++ DatabaseExplorer/DatabaseExplorer/ViewController.swift
@@ -21,7 +21,92 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
+
+//extension ViewController: NSOutlineViewDataSource {
+//    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+//        return DataModel.shared.fetchUniversities().count
+//    }
+//
+//    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+//        if let feed = item as? Feed {
+//            return feed.children[index]
+//        }
+//
+//        return feeds[index]
+//    }
+//
+//    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+//        if let feed = item as? Feed {
+//            return feed.children.count > 0
+//        }
+//
+//        return false
+//    }
+//}
+//
+//extension ViewController: NSOutlineViewDelegate {
+//    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+//        var view: NSTableCellView?
+//        //1
+//        if let feed = item as? Feed {
+//            if tableColumn?.identifier == "DateColumn" {
+//                view = outlineView.make(withIdentifier: "DateCell", owner: self) as? NSTableCellView
+//                if let textField = view?.textField {
+//                    textField.stringValue = ""
+//                    textField.sizeToFit()
+//                }
+//            } else {
+//                view = outlineView.make(withIdentifier: "FeedCell", owner: self) as? NSTableCellView
+//                if let textField = view?.textField {
+//                    textField.stringValue = feed.name
+//                    textField.sizeToFit()
+//                }
+//            }
+//        } else if let feedItem = item as? FeedItem {
+//            //1
+//            if tableColumn?.identifier == "DateColumn" {
+//                //2
+//                view = outlineView.make(withIdentifier: "DateCell", owner: self) as? NSTableCellView
+//
+//                if let textField = view?.textField {
+//                    //3
+//                    textField.stringValue = dateFormatter.string(from: feedItem.publishingDate)
+//                    textField.sizeToFit()
+//                }
+//            } else {
+//                //4
+//                view = outlineView.make(withIdentifier: "FeedItemCell", owner: self) as? NSTableCellView
+//                if let textField = view?.textField {
+//                    //5
+//                    textField.stringValue = feedItem.title
+//                    textField.sizeToFit()
+//                }
+//            }
+//        }
+//        //More code here
+//        return view
+//    }
+//
+//    func outlineViewSelectionDidChange(_ notification: Notification) {
+//        //1
+//        guard let outlineView = notification.object as? NSOutlineView else {
+//            return
+//        }
+//
+//        //2
+//        let selectedIndex = outlineView.selectedRow
+//
+//        if let feedItem = outlineView.item(atRow: selectedIndex) as? FeedItem {
+//            //3
+//            let url = URL(string: feedItem.url)
+//            //4
+//            if let url = url {
+//                //5
+//                self.webView.mainFrame.load(URLRequest(url: url))
+//            }
+//        }
+//    }
+//}
+
 
