3b29f34d5dd812d377851608408c7f07f2ca970f
Rostyslav Stakhiv <rostyslav.stakhiv@kindgeek.com>
2018-01-23 12:17:53 +0200

    - Setup scrollview - Created FromVC

---
+++ DatabaseExplorer/DatabaseExplorer/CustomUI/Windows/FormVC.swift
@@ -0,0 +1,19 @@
+//
+//  FormVC.swift
+//  DatabaseExplorer
+//
+//  Created by KindGeek on 1/23/18.
+//  Copyright © 2018 rstakhiv. All rights reserved.
+//
+
+import Cocoa
+
+class FormVC: NSViewController {
+
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
